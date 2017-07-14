f875c2b8b55694ba785df548987440e86cf0111e
yinxi <348718026@qq.com>
2017-07-14 11:04:26 +0800

    111

--- src/main/java/com/giit/www/system/controller/LoginController.java
+++ src/main/java/com/giit/www/system/controller/LoginController.java
@@ -30,7 +30,7 @@ public class LoginController {
         String exceptionClassName = (String) req.getAttribute("shiroLoginFailure");
         String error = null;
         if (UnknownAccountException.class.getName().equals(exceptionClassName)) {
-            error = "用户名/密码错误---localhost";
+            error = "用户名/密码错误---localhost1";
         } else if (IncorrectCredentialsException.class.getName().equals(exceptionClassName)) {
             error = "用户名/密码错误---修改11";
         } else if (exceptionClassName != null) {
