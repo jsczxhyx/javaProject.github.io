b5c59d79a547d1aff02300f3e50574d77db901de
jsczxhyx <348718026@qq.com>
2017-07-14 11:02:21 +0800

    Update LoginController.java

--- src/main/java/com/giit/www/system/controller/LoginController.java
+++ src/main/java/com/giit/www/system/controller/LoginController.java
@@ -30,7 +30,7 @@ public class LoginController {
         String exceptionClassName = (String) req.getAttribute("shiroLoginFailure");
         String error = null;
         if (UnknownAccountException.class.getName().equals(exceptionClassName)) {
-            error = "用户名/密码错误---local";
+            error = "用户名/密码错误---git";
         } else if (IncorrectCredentialsException.class.getName().equals(exceptionClassName)) {
             error = "用户名/密码错误---修改11";
         } else if (exceptionClassName != null) {
